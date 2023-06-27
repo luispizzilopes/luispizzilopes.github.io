3046d670a791e43bc68111050d3d8df48cb2f3c0
Luis Felipe Pizzi Lopes <101680647+luispizzilopes@users.noreply.github.com>
2023-06-27 14:22:03 -0300

    Add files via upload

--- arvorelink-pv/script.js
+++ arvorelink-pv/script.js
@@ -1,6 +1,8 @@
 const buttonWhatsapp = document.querySelector('#whatsapp'); 
 const buttonInstagram = document.querySelector('#instagram'); 
 const buttonFacebook = document.querySelector('#facebook'); 
+const buttonLinkedin = document.querySelector('#linkedin'); 
+const linkDesenvolvedor = document.querySelector('.desenvolvedor'); 
 
 buttonWhatsapp.addEventListener('click', ()=>{
     window.open('https://api.whatsapp.com/send?phone=5516981457144');
@@ -12,4 +14,12 @@ buttonInstagram.addEventListener('click', ()=>{
 
 buttonFacebook.addEventListener('click', ()=>{
     window.open('https://www.facebook.com/paulo.pizzi.3'); 
+});
+
+buttonLinkedin.addEventListener('click', ()=>{
+    window.open('https://www.linkedin.com/in/paulo-victor-pizzi/');
+})
+
+linkDesenvolvedor.addEventListener('click', ()=>{
+    window.open('https://www.linkedin.com/in/luis-felipe-pizzi-lopes-04b531204/'    ); 
 });
